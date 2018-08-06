0404b98ffc48f325a08e8ab104fc93fa843067aa
engadel <dola.ezz1@gmail.com>
2018-08-06 17:10:32 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {UserComponent} from './components/user/user.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+
+import {DataService} from './services/data.service';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        UserComponent,
+        NavbarComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [DataService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
